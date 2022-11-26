69f86fa75e6c7484af5a7f814e8149005f79133c
Lena <lenna.socials@gmail.com>
2022-11-26 17:17:32 -0300

    clase 9

---
+++ semana6/Miercoles/codigo.js
@@ -0,0 +1,13 @@
+// variables
+// una variable es un contenedor de informacion
+
+let pais = 'España';
+let continente = 'Europa';
+let antiguedad = '2021';
+let pais_y_continente = pais + ' ' + continente;
+
+pais = 'Mexico';
+continente = 'Latinoamerica';
+
+console.log(pais,continente,antiguedad);
+alert(pais_y_continente)
